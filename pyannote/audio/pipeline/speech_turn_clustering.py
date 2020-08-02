384103a91147a3f854571d2e9fc4168b7342be0d
Sean Nam <seannam@protonmail.com>
2020-08-01 17:28:43 -0700

    write turn data to file

--- pyannote/audio/pipeline/speech_turn_clustering.py
+++ pyannote/audio/pipeline/speech_turn_clustering.py
@@ -211,9 +211,11 @@ class SpeechTurnClustering(Pipeline):
         for l, label in enumerate(skipped_labels):
             mapping[label] = -(l + 1)
         
-        print('mapping:\n', mapping) #added
         # do the actual mapping
         print('speech_turns:\n:', speech_turns.rename_labels(mapping=mapping)) #added
+        with open('test.txt', 'w') as f:
+            f.write(speech_turns.rename_labels(mapping=mapping))
+            print('writing successful!')
         return speech_turns.rename_labels(mapping=mapping)
 
     def __call__(
